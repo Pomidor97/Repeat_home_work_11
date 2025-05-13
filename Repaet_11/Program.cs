6d096d76584e58bba4c3141d4522a1d071630367
Zhassulan.Suleiman <Zhassulan.suleiman@gmail.com>
2025-05-13 11:13:46 +0500

    Task_1

---
+++ Repaet_11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your surname:");
+string surname = Console.ReadLine();
+Console.WriteLine("Enter the year of birth:");
+int ear = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your born place");
+string bornPlace = Console.ReadLine();
+
+
+Console.WriteLine($"Hello, {surname} {name}. You are {2025 - ear} years old. Your born place is {bornPlace}");
+
+
